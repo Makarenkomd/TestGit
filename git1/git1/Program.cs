e674d567ee6c101b021a2e9c1b69bf161400d6d7
MD <mahadrum@yandex.ru>
2018-09-10 13:56:30 +0300

    Написана вторая строка кода
    Signed-off-by: MD <mahadrum@yandex.ru>

--- git1/git1/Program.cs
+++ git1/git1/Program.cs
@@ -11,6 +11,7 @@ namespace git1
         static void Main(string[] args)
         {
             Console.WriteLine("Проверка связи");
+            Console.WriteLine("Изменения в программе");
         }
     }
 }
